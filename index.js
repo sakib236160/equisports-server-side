8ccf91b75cae128eb5208f191062f8dc031e74af
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-03-03 23:45:57 +0600

    Delete Button Done

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app=express();
-const port =process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k7k1l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,50 +18,65 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const sportCollection = client.db("sportDB").collection("sport");
 
-    const sportCollection = client.db('sportDB').collection('sport');
-
-    app.get('/all-products', async (req, res) => {
-        try {
-            const products = await sportCollection.find().toArray(); 
-            res.json(products);
-        } catch (error) {
-            res.status(500).json({ error: "Something went wrong" });
-        }
-    });
-    
-
-    app.get('/limited-products', async (req, res) => {
-        try {
-            const products = await sportCollection.find().limit(6).toArray();
-            res.json(products);
-        } catch (error) {
-            res.status(500).json({ error: "Something went wrong" });
-        }
+    // all Products data
+    app.get("/all-products", async (req, res) => {
+      try {
+        const products = await sportCollection.find().toArray();
+        res.json(products);
+      } catch (error) {
+        res.status(500).json({ error: "Something went wrong" });
+      }
     });
 
-    
+    // limited-products data
+    app.get("/limited-products", async (req, res) => {
+      try {
+        const products = await sportCollection.find().limit(6).toArray();
+        res.json(products);
+      } catch (error) {
+        res.status(500).json({ error: "Something went wrong" });
+      }
+    });
 
-    app.post('/equipment',async(req,res)=>{
-        const newEquipment=req.body;
-        console.log(newEquipment);
-        const result = await sportCollection.insertOne(newEquipment);
+    app.get("/equipment", async(req,res)=>{
+        const cursor= sportCollection.find();
+        const result= await cursor.toArray();
         res.send(result);
     })
-    
 
+    app.get("/equipment/:id", async(req,res)=>{
+      const id = req.params.id;
+    })
 
+    // Add equipment
+    app.post("/equipment", async (req, res) => {
+      const newEquipment = req.body;
+      console.log(newEquipment);
+      const result = await sportCollection.insertOne(newEquipment);
+      res.send(result);
+    });
+    // delete
+    app.delete("/equipment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await sportCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -73,16 +84,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("equisports server is running............");
+});
 
-
-
-
-app.get('/',(req,res)=>{
-    res.send('equisports server is running............')
-})
-
-
-
-app.listen(port,()=>{
-    console.log(`equisports server is on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`equisports server is on port:${port}`);
+});
