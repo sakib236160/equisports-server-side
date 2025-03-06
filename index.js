3ba9d5cad8faeb7f46589a3da7fe9326ea40aa06
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-03-06 13:40:06 +0600

    creating a database collaection for users done and user getting done form client side

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
     const sportCollection = client.db("sportDB").collection("sport");
 
     // all Products data
@@ -102,7 +102,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+   
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
